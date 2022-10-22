153c87476d908ff51c082a99e6f41b07662cd182
sivaprasad <sivaprasadtppba@gmail.com>
2022-10-22 23:37:48 +0530

    styling done

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 let handleSubmit = (event) => {
   event.preventDefault();
-  let name = document.getElementById("name")?.value;
-  let gender = document.getElementById("gender")?.value;
-  let studentClass = document.getElementById("class")?.value;
-  let division = document.getElementById("division")?.value;
+  let name = document.getElementById("name")?.value.trim();
+  let gender = document.getElementById("gender")?.value.trim();
+  let studentClass = document.getElementById("class")?.value.trim();
+  let division = document.getElementById("division")?.value.trim();
   let dob = document.getElementById("dob")?.value;
   if (name && gender && studentClass && division && dob) {
     alert("success");
-  } else {
-    alert("field missing")
+  }
+  if(!name) {
+    document.getElementById('name').classList.add("error");
+  }
+  if(!gender) {
+    document.getElementById('gender').classList.add("error");
+  }
+  if(!studentClass) {
+    document.getElementById('class').classList.add("error");
+  }
+  if(!division) {
+    document.getElementById('division').classList.add("error");
+  }
+  if(!dob) {
+    document.getElementById('dob').classList.add("error");
   }
   console.log(name, gender, studentClass, division, dob);
 };
