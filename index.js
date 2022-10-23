2f2717f42e22c431e6b1b0e377d3a86f6d4c8b5b
sivaprasad <sivaprasadtppba@gmail.com>
2022-10-23 11:58:00 +0530

    stying completed error message added

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
 let handleSubmit = (event) => {
   event.preventDefault();
-  let name = document.getElementById("name")?.value.trim();
-  let gender = document.getElementById("gender")?.value.trim();
-  let studentClass = document.getElementById("class")?.value.trim();
-  let division = document.getElementById("division")?.value.trim();
-  let dob = document.getElementById("dob")?.value;
+  let name = document.forms["form"]["name"].value.trim();
+  let gender = document.forms["form"]["gender"].value;
+  let studentClass = document.forms["form"]["class"].value;
+  let division = document.forms["form"]["division"].value.trim();
+  let dob = document.forms["form"]["dob"].value;
   if (name && gender && studentClass && division && dob) {
-    alert("success");
+    alert(
+      "Name: " +
+        name +
+        "\nGender: " +
+        gender +
+        "\nClass: " +
+        studentClass +
+        "\nDivision:" +
+        division +
+        "\nDob: " +
+        dob
+    );
   }
-  if(!name) {
-    document.getElementById('name').classList.add("error");
-  }
-  if(!gender) {
-    document.getElementById('gender').classList.add("error");
-  }
-  if(!studentClass) {
-    document.getElementById('class').classList.add("error");
-  }
-  if(!division) {
-    document.getElementById('division').classList.add("error");
-  }
-  if(!dob) {
-    document.getElementById('dob').classList.add("error");
-  }
-  console.log(name, gender, studentClass, division, dob);
+  name
+    ? document.getElementById("name").classList.remove("error")
+    : document.getElementById("name").classList.add("error");
+  studentClass
+    ? document.getElementById("class").classList.remove("error")
+    : document.getElementById("class").classList.add("error");
+  division
+    ? document.getElementById("division").classList.remove("error")
+    : document.getElementById("division").classList.add("error");
+  dob
+    ? document.getElementById("dob").classList.remove("error")
+    : document.getElementById("dob").classList.add("error");
+  gender
+    ? document.getElementById("error-message").classList.remove("error-message-active")
+    : document.getElementById("error-message").classList.add("error-message-active");
 };
